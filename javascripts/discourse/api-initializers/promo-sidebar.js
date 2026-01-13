fd11e30fb8c94911a3855ea1d2130e9623f07284
Shah <xiahamuhamaiti@sabanciuniv.edu>
2026-01-13 16:23:09 -0500

    Promo card: use script injection instead of Glimmer component, lower breakpoint

--- javascripts/discourse/api-initializers/promo-sidebar.js
+++
@@ -1,6 +0,0 @@
-import { apiInitializer } from "discourse/lib/api";
-import PromoSidebarCard from "../components/promo-sidebar-card";
-
-export default apiInitializer("1.0.0", (api) => {
-  api.renderInOutlet("above-main-container", PromoSidebarCard);
-});
