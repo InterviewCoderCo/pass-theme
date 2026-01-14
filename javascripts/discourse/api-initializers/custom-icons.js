98af16454f9358065f284a6e12318295b49d83bd
Shah <shah.nur@co-one.co>
2026-01-14 18:40:41 -0500

    Replace filled icons with outline versions (far-bookmark, far-flag, far-bell)

--- javascripts/discourse/api-initializers/custom-icons.js
+++ javascripts/discourse/api-initializers/custom-icons.js
@@ -5,4 +5,9 @@ export default apiInitializer((api) => {
   api.replaceIcon("layer-group", "custom-topics");   // Topics
   api.replaceIcon("inbox", "custom-messages");       // My Messages
   api.replaceIcon("user", "custom-my-posts");        // My Posts
+
+  // Replace topic footer button icons with outline versions
+  api.replaceIcon("bookmark", "far-bookmark");       // Bookmark
+  api.replaceIcon("flag", "far-flag");               // Flag
+  api.replaceIcon("bell", "far-bell");               // Tracking/Notifications
 });
