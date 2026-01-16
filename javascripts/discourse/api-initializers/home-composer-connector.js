1901782459e81a9a1001d18dfed305b76a3ba9cd
LittleKingShah <xiahamuhamaiti@sabanciuniv.edu>
2026-01-16 12:24:33 -0500

    Revert "Add home composer widget for quick topic creation"
    This reverts commit 5588a624e1cdadcc78d6c7cf173199a2e8140fa0.

--- javascripts/discourse/api-initializers/home-composer-connector.js
+++
@@ -1,6 +0,0 @@
-import { apiInitializer } from "discourse/lib/api";
-import HomeComposer from "../components/home-composer";
-
-export default apiInitializer("1.0", (api) => {
-  api.renderInOutlet("discovery-above", HomeComposer);
-});
